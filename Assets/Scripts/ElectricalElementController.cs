9b3ed37f6b7a6660a9eda57eeec3bb9ee7aceb30
Matt Kersey <mkersey95@gmail.com>
2021-03-13 15:56:14 -0500

    initial commit

---
+++ Assets/Scripts/ElectricalElementController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricalElementController : MonoBehaviour
+{
+	public GameObject endpoint0;
+	public GameObject endpoint1;
+
+	public GameObject[] connections1;
+	public GameObject[] connections2;
+
+	public float voltage;
+	public float current;
+	public float resistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WireController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireController : ElectricalElementController
+{
+	public GameObject cylinder;
+	private float thickness;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thickness = cylinder.transform.localScale.x;
+        voltage = 0;
+        resistance = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 avgPos = 0.5f * (endpoint0.transform.localPosition + endpoint1.transform.localPosition);
+        Vector3 difference = endpoint0.transform.localPosition - endpoint1.transform.localPosition;
+        float length = 0.5f * difference.magnitude;
+        Quaternion rotation = new Quaternion();
+        rotation.SetFromToRotation(Vector3.up, difference);
+        cylinder.transform.localRotation = rotation;
+        cylinder.transform.localPosition = avgPos;
+        cylinder.transform.localScale = new Vector3(thickness, length, thickness);
+    }
+}
