c95e4dc8f24a0a253b130f6b9a6db383b7394b2f
Matt Kersey <mkersey95@gmail.com>
2021-03-15 10:42:52 -0400

    Some more script work

--- Assets/Scripts/ElectricalElementController.cs
+++ Assets/Scripts/ElectricalElementController.cs
@@ -1,27 +1,52 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ElectricalElementController : MonoBehaviour
 {
+	public GameObject vertexPrefab;
+
 	public GameObject endpoint0;
 	public GameObject endpoint1;
-
-	public GameObject[] connections1;
-	public GameObject[] connections2;
+	public GameObject vertex0;
+	public GameObject vertex1;
 
 	public float voltage;
 	public float current;
 	public float resistance;
-    // Start is called before the first frame update
-    void Start()
+	public float snapThreshold;
+
+	private Vector3 snapPosition;
+	private Quaternion snapRotation;
+
+    void GrabStart()
     {
-        
+    	snapPosition = this.transform.position;
+    	snapRotation = this.transform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    void SnapOrDelete()
     {
-        
+    	if ((this.transform.position - snapPosition).magnitude > snapThreshold) {
+    		destroy(gameObject);
+    	} else {
+    		this.transform.position = snapPosition;
+    		this.transform.rotation = snapRotation;
+    	}
+    }
+
+    void EndManipulation()
+    {
+    	// Instantiate new wires at vertices
+    	if (vertex1 != null) {
+    		endpoint1.position = vertex1.position;
+    	} else {
+    		Instantiate(vertexPrefab, endpoint1.position, Quaternion.identity);
+    	}
+    	if (vertex0 != null) {
+    		endpoint0.position = vertex0.position;
+    	} else {
+    		Instantiate(vertexPrefab, endpoint0.position, Quaternion.identity);
+    	}
     }
 }
